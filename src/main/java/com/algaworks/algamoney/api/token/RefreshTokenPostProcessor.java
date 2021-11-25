3f52c4128c6b7c59e730d7fbb13a6417e8b05cf9
Cássio Coelho <cassioozn@gmail.com>
2021-11-25 10:37:40 -0300

    commit antes da Aula 6.9

--- src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -49,7 +49,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setHttpOnly(true);
         refreshTokenCookie.setSecure(false);    //TODO : Mudar para true em produção
-        refreshTokenCookie.setPath(req.getContextPath() + "/auth/token");
+        refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
         refreshTokenCookie.setMaxAge(1800);
         resp.addCookie(refreshTokenCookie);
     }
