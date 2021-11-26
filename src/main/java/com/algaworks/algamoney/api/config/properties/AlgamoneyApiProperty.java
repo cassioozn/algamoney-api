4257b44fb3bdbfd4264cafdcb71077120b9987ff
Cássio Coelho <cassioozn@gmail.com>
2021-11-26 15:04:25 -0300

    commit antes da aula 7.3

---
+++ src/main/java/com/algaworks/algamoney/api/config/properties/AlgamoneyApiProperty.java
@@ -0,0 +1,35 @@
+package com.algaworks.algamoney.api.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties("algamoney")
+public class AlgamoneyApiProperty {
+
+    String originPermitida = "";
+    final Seguranca seguranca = new Seguranca();
+
+    public Seguranca getSeguranca() {
+        return seguranca;
+    }
+
+    public String getOriginPermitida() {
+        return originPermitida;
+    }
+
+    public void setOriginPermitida(String originPermitida) {
+        this.originPermitida = originPermitida;
+    }
+
+    public static class Seguranca{
+        boolean enableHttps;
+
+        public boolean isEnableHttps() {
+            return enableHttps;
+        }
+
+        public void setEnableHttps(boolean enableHttps) {
+            this.enableHttps = enableHttps;
+        }
+    }
+}
--- src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -1,5 +1,7 @@
 package com.algaworks.algamoney.api.token;
 
+import com.algaworks.algamoney.api.config.properties.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -20,6 +22,8 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
 
+    @Autowired
+    AlgamoneyApiProperty algamoneyApiProperty;
 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -48,7 +52,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
     private void adicionarRefreshTokenCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(false);    //TODO : Mudar para true em produção
+        refreshTokenCookie.setSecure(algamoneyApiProperty.getSeguranca().isEnableHttps());
         refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
         refreshTokenCookie.setMaxAge(1800);
         resp.addCookie(refreshTokenCookie);
