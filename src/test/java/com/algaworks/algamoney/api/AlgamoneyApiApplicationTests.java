04a8e4ad3b67ab1e5640cfa407049cd270b37060
Cássio Coelho <cassioozn@gmail.com>
2021-11-29 10:54:23 -0300

    commit durante aula 7.4

--- src/test/java/com/algaworks/algamoney/api/AlgamoneyApiApplicationTests.java
+++ src/test/java/com/algaworks/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -3,6 +3,8 @@ package com.algaworks.algamoney.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//https://algamoneyalgamoney-api.herokuapp.com/
+
 @SpringBootTest
 class AlgamoneyApiApplicationTests {
 
