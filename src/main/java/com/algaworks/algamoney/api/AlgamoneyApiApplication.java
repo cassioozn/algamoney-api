2478c526fa0ad84e2716d3eac47529574dc3a4e9
Cássio Coelho <cassioozn@gmail.com>
2021-11-29 11:34:32 -0300

    Mais commit durante aula 7.4

--- src/main/java/com/algaworks/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/algaworks/algamoney/api/AlgamoneyApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+//https://algamoneyalgamoney-api.herokuapp.com/
+
+
 @SpringBootApplication
 @EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
